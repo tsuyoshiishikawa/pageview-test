34ad16d859dd644abc272a3a3b7fab4ec972fee9
tsuyoshi ishikawa <tsuyoshiishikawa@lavan7-no-macbook-pro.local>
2016-03-05 01:48:06 +0900

    Initial Commit

---
+++ pageview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pageview
+//
+//  Created by tsuyoshi ishikawa on 2016/03/05.
+//  Copyright © 2016年 tsuyoshi ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
